01a6abd73fce470c304a886c3fd3568568ed8a89
jsyoon <sunho325@gmail.com>
2019-04-02 11:53:18 +0900

    1

--- ControlClient.py
+++ ControlClient.py
@@ -22,17 +22,17 @@ class CtrClient:
                 break
 
             clientLists = pickle.loads(data)
-            #print(clientLists)
+            #print(self.currClients)
             if(len(clientLists) > len(self.currClients)):
                 res = clientLists.keys() - self.currClients.keys()
                 newclients = {k:clientLists[k] for k in res}
-                self.currClients.update(newclients)
+                #self.currClients.update(newclients)
 
                 for key,value in newclients.items():
                     self.clientCount = self.clientCount+1
-                    #gui.addClientRButton(key, self.clientCount)
-                    gui.addClientButton(key)
-                print(newclients)
+                    gui.addClientButton(key,value)
+                #print(newclients)
+            self.currClients.update(clientLists)
 
     def sendMsg(self,val):
         self.sock.send(val)
@@ -46,6 +46,8 @@ class CtrGui:
         self.v = IntVar()
         self.v.set(1)  # initializing the choice, i.e. Python
 
+        self.detailButton = 0
+
         #self.master.grid_rowconfigure(0, weight=1)
         #self.master.grid_columnconfigure(0, weight=1)
         self.win_colour = '#D2B48C'
@@ -57,10 +59,13 @@ class CtrGui:
         self.frame2.pack(side="right", fill="both", expand=True)
 
 
-    def addClientButton(self, ButtonID):
+    def addClientButton(self, ButtonID,value):
         buttonx = Button(self.frame1, text=ButtonID, fg="red", width=15)
         buttonx.grid()
         buttonx.config(command=lambda:self.showDetail(ButtonID))
+        if(self.detailButton == 0):
+            self.makeDetailButton(ButtonID,value)
+            self.detailButton = 1
 
 
     def showChoice(self, Label, key, key1):
@@ -103,24 +108,50 @@ class CtrGui:
 
 
     def showDetail(self,key):
-        Button(self.frame2,text='Power A',width=12,command=lambda:self.showChoice(labelPA, key, 'PA')).grid(row=0,column=0)
-        Button(self.frame2,text='Power B',width=12,command=lambda:self.showChoice(labelPB, key, 'PB')).grid(row=0,column=1)
-        Button(self.frame2,text='LED',width=12,command=lambda:self.showChoice(labelLED, key, 'LED')).grid(row=0,column=2)
-        Button(self.frame2,text='STYLE',width=12,command=lambda:self.showChoice(labelSTYLE, key, 'STYLE')).grid(row=0,column=3)
+        self.buttonPA.config(command=lambda:self.showChoice(self.labelPA, key, 'PA'))
+        self.buttonPB.config(command=lambda:self.showChoice(self.labelPB, key, 'PB'))
+        self.buttonLED.config(command=lambda:self.showChoice(self.labelLED, key, 'LED'))
+        self.buttonSTYLE.config(command=lambda:self.showChoice(self.labelSTYLE, key, 'STYLE'))
 
         text1 = 'OFF' if(CtrClient.currClients[key]['PA'] == 0) else 'ON'
-        labelPA=Label(self.frame2,relief=RIDGE,text=text1,width=12)
-        labelPA.grid(row=1,column=0)
-        text1 = 'OFF' if(CtrClient.currClients[key]['PB'] == 0) else 'ON'
-        labelPB=Label(self.frame2,relief=RIDGE,text=text1,width=12)
-        labelPB.grid(row=1,column=1)
-        text1 = 'OFF' if(CtrClient.currClients[key]['LED'] == 0) else 'ON'
-        labelLED=Label(self.frame2,relief=RIDGE,text=text1,width=12)
-        labelLED.grid(row=1,column=2)
-        text1 = str(CtrClient.currClients[key]['STYLE'])
-        labelSTYLE=Label(self.frame2,relief=RIDGE,text=text1,width=12)
-        labelSTYLE.grid(row=1,column=3)
+        self.labelPA.config(text=text1)
 
+        text1 = 'OFF' if(CtrClient.currClients[key]['PB'] == 0) else 'ON'
+        self.labelPB.config(text=text1)
+        text1 = 'OFF' if(CtrClient.currClients[key]['LED'] == 0) else 'ON'
+        self.labelLED.config(text=text1)
+        text1 = str(CtrClient.currClients[key]['STYLE'])
+        self.labelSTYLE.config(text=text1)
+
+    def makeDetailButton(self,key,value):
+        self.buttonPA = Button(self.frame2,text='Power A',width=12)
+        self.buttonPA.grid(row=0,column=0)
+        self.buttonPA.config(command=lambda:self.showChoice(self.labelPA, key, 'PA'))
+
+        self.buttonPB = Button(self.frame2,text='Power B',width=12)
+        self.buttonPB.grid(row=0,column=1)
+        self.buttonPB.config(command=lambda:self.showChoice(self.labelPB, key, 'PB'))
+
+        self.buttonLED = Button(self.frame2,text='LED',width=12)
+        self.buttonLED.grid(row=0,column=2)
+        self.buttonLED.config(command=lambda:self.showChoice(self.labelLED, key, 'LED'))
+
+        self.buttonSTYLE = Button(self.frame2,text='STYLE',width=12)
+        self.buttonSTYLE.grid(row=0,column=3)
+        self.buttonSTYLE.config(command=lambda:self.showChoice(self.labelSTYLE, key, 'STYLE'))
+
+        text1 = 'OFF' if(value['PA'] == 0) else 'ON'
+        self.labelPA=Label(self.frame2,relief=RIDGE,text=text1,width=12)
+        self.labelPA.grid(row=1,column=0)
+        text1 = 'OFF' if(value['PB'] == 0) else 'ON'
+        self.labelPB=Label(self.frame2,relief=RIDGE,text=text1,width=12)
+        self.labelPB.grid(row=1,column=1)
+        text1 = 'OFF' if(value['LED'] == 0) else 'ON'
+        self.labelLED=Label(self.frame2,relief=RIDGE,text=text1,width=12)
+        self.labelLED.grid(row=1,column=2)
+        text1 = str(value['STYLE'])
+        self.labelSTYLE=Label(self.frame2,relief=RIDGE,text=text1,width=12)
+        self.labelSTYLE.grid(row=1,column=3)
 
 root = Tk()
 client = CtrClient(('localhost',3100))
